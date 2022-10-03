df66c1281da31736189aaab8dc0bba291cd7386a
Aleksandr-Kovshar <aleksandr.mpro@gmail.com>
2022-10-04 00:45:49 +0300

    add task 3 beta

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+    form: document.querySelector ('.feedback-form'),
+    textarea: document.querySelector ('.feedback-form textarea'),
+}
+
+
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+function onTextareaInput(evt){
+   const message = evt.target.value;
+   console.log(message);
+   localStorage.setItem(STORAGE_KEY, message);
+}
+
+
+function popularTextarea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    
+        if(savedMessage)
+        { 
+            console.log(savedMessage);
+            refs.textarea.value = savedMessage;
+        }
+    }
+    popularTextarea()
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt){
+    evt.preventDefault();
+    console.log('Отправляем форму');
+    evt.currentTarget.reset()
+    localStorage.removeItem(STORAGE_KEY)
+}
+
+
+
+
+
+// JSON.stringify(value)
+// JSON.parse(value)
