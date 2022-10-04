31df2ad506eba0aa999f48aa9f5976d9af145c69
Aleksandr-Kovshar <aleksandr.mpro@gmail.com>
2022-10-04 22:22:50 +0300

    8 task 3 done
    add 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,29 +2,34 @@ import throttle from 'lodash.throttle';
 
 
 const STORAGE_KEY = 'feedback-form-state';
+let formData = {};
+
+
 
 const refs = {
     form: document.querySelector ('.feedback-form'),
-    textarea: document.querySelector ('.feedback-form textarea'),
+    formEmail: document.querySelector ('[type="email"]'),
+    formMessage: document.querySelector ('.feedback-form textarea'),
 }
 
 
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
 
 function onTextareaInput(evt){
-   const message = evt.target.value;
-   console.log(message);
-   localStorage.setItem(STORAGE_KEY, message);
-}
+    formData[evt.target.name]=evt.target.value;
+    console.log(formData);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    
+ }
 
 
 function popularTextarea() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY);
-    
-        if(savedMessage)
+    const savedformData = JSON.parse (localStorage.getItem(STORAGE_KEY));
+        if(savedformData)
         { 
-            console.log(savedMessage);
-            refs.textarea.value = savedMessage;
+            console.log(savedformData);
+            refs.formEmail.value = savedformData.email;
+            refs.formMessage.value = savedformData.message;
         }
     }
     popularTextarea()
@@ -33,14 +38,10 @@ refs.form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(evt){
     evt.preventDefault();
-    console.log('Отправляем форму');
-    evt.currentTarget.reset()
+    console.log(formData);
+    evt.target.reset()
     localStorage.removeItem(STORAGE_KEY)
 }
 
 
 
-
-
-// JSON.stringify(value)
-// JSON.parse(value)
